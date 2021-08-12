8f38469f131c301f4da5d2f081258abe4b8b34ca
Abel <hi@abelcastro.dev>
2021-08-12 05:03:51 +0200

    Improve route importer error handling

--- app/routes/management/commands/import_routes.py
+++ app/routes/management/commands/import_routes.py
@@ -7,6 +7,14 @@ from routes.models import Route
 
 
 class Command(BaseCommand):
+    """
+    Create Route object from json.
+
+    The json data needs to have this format in order to be
+    displayed  properly in the map:
+
+    {"polyline": [[[lat, lng], [lat, lng], ...]]}
+    """
     help = "Import routes from a JSON file"
 
     @staticmethod
@@ -18,17 +26,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = options["file_path"]
+
         with open(file_path) as json_file:
             routes = json.load(json_file)
             json_file.close()
 
         imported_routes = 0
+
+        if isinstance(routes, dict):
+            routes = [routes]
+
         for data in routes:
-            route, created = Route.objects.get_or_create(
-                data=data, defaults={"name": self._create_route_name()}
-            )
+            try:
+                data = data.get("polyline")[0]
+                route, created = Route.objects.get_or_create(
+                    data=data, defaults={"name": self._create_route_name()}
+                )
 
-            if created:
-                imported_routes += 1
-
+                if created:
+                    imported_routes += 1
+            except IndexError:
+                print("One route could not be imported")
+                continue
         print(f"{imported_routes} routes were imported")
--- app/routes/tests/test_commands.py
+++ app/routes/tests/test_commands.py
@@ -5,11 +5,29 @@ from routes.models import Route
 
 
 @mark.django_db
-def test_import_routes():
-    call_command("import_routes", "routes/tests/test_data.json")
+def test_import_routes__no_duplicates():
+    """
+    Verify that the importer data is correct and that objects
+    """
+    call_command("import_routes", "routes/tests/test_data/multiple_routes.json")
 
     assert Route.objects.count() == 2
 
-    call_command("import_routes", "routes/tests/test_data.json")
+    call_command("import_routes", "routes/tests/test_data/multiple_routes.json")
 
     assert Route.objects.count() == 2
+
+
+@mark.django_db
+def test_import_routes__values():
+    expected_data = [[16.354, 48.27571], [16.35444, 48.27522]]
+    call_command("import_routes", "routes/tests/test_data/single_route.json")
+    route = Route.objects.get()
+
+    assert route.data == expected_data
+
+
+@mark.django_db
+def test_import_routes__wrong_data():
+    call_command("import_routes", "routes/tests/test_data/single_route_wrong_data.json")
+    assert Route.objects.count() == 0
